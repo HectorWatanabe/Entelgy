1a3e97e2f9edaa63c8cf65b361903b8f26886f13
Héctor Chumpitaz <hectortrabajoscw@gmail.com>
2021-05-07 16:54:57 -0500

    Spring Initializr

---
+++ src/test/java/com/hector/entelgy/EntelgyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hector.entelgy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntelgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
